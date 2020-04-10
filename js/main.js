3a3df457504cefcd2de69eecd638f7f4dc55c764
Jake Gadaleta <jake.gads@gmail.com>
2020-04-10 00:18:40 -0400

    working on the color switcher

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 document.body.background = "../Images/wall.jpg";
+
+light_mode = true
+
+mode = document.getElementById("colorMode");
+
+let swap = function() {
+    if (light_mode) {
+        light_mode = false;
+        mode.background = "../Images/sunl.png";
+        document.body.background = "../Images/walld.jpg";
+
+        recolor(document.getElementsByClassName("rounded_nav"), "#5A4F4F", "blue");
+
+
+    } else {
+        light_mode = true;
+        mode.background = "../Images/sund.png";
+        document.body.background = "../Images/wall.jpg";
+    }
+}
+
+let recolor = function(array, backgroundColor, textColor) {
+    for (let i = 0; i < array.length; i++) {
+        alert("We made it here Jake")
+        array[i].background = backgroundColor;
+        array[i].color = textColor;
+    }
+}
