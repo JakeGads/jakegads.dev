5a3ee13c58ac4ed0708923e03a4b8bdb3eeec5fc
Jake Gadaleta <jake.gads@gmail.com>
2020-04-12 21:12:35 -0400

    finished the fll page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+/*jslint es6:true*/
+
+
 document.body.background = "../Images/wall.jpg";
 
-light_mode = new Date().getHours() > 20;
+light_mode = new Date().getHours() >= 20;
 
 mode = document.getElementById("colorMode");
 
@@ -10,25 +13,33 @@ let swap = function() {
         document.body.background = "../Images/walld.jpg";
         mode.innerHTML = "<h2>Dark Mode</h2>";
 
-        recolor(document.getElementsByClassName("rounded_nav"), "#5A4F4F", "white");
-        recolor(document.getElementsByClassName("page_head"), "#5A4F4F", "white");
-        recolor(document.getElementsByClassName("bubble"), "#5A4F4F", "white");
-        recolor(document.getElementsByClassName("rounded_footer"), "#5A4F4F", "white")
+        recolor(document.getElementsByClassName("rounded_nav"), "white", "#5A4F4F");
+        recolor(document.getElementsByClassName("page_head"), "white", "#5A4F4F");
+        recolor(document.getElementsByClassName("bubble"), "white", "#5A4F4F");
+        recolor(document.getElementsByClassName("rounded_footer"), "white", "#5A4F4F");
+        recolor(document.getElementsByClassName("nav_links"), "white", false)
     } else {
         light_mode = true;
         document.body.background = "../Images/wall.jpg";
         mode.innerHTML = "<h2>Light Mode</h2>";
 
-        recolor(document.getElementsByClassName("rounded_nav"), "white", "black");
-        recolor(document.getElementsByClassName("page_head"), "white", "black");
-        recolor(document.getElementsByClassName("bubble"), "white", "black");
-        recolor(document.getElementsByClassName("rounded_footer"), "white", "black");
+        recolor(document.getElementsByClassName("rounded_nav"), "black", "white");
+        recolor(document.getElementsByClassName("page_head"), "black", "white");
+        recolor(document.getElementsByClassName("bubble"), "black", "white");
+        recolor(document.getElementsByClassName("rounded_footer"), "black", "white");
+        recolor(document.getElementsByClassName("nav_links"), "black", false);
     }
-}
+};
 
-let recolor = function(array, backgroundColor, textColor) {
-    for (let i = 0; i < array.length; i++) {
-        array[i].style.backgroundColor = backgroundColor;
-        array[i].style.color = textColor;
+let recolor = function(array, textColor, backgroundColor) {
+    for (i of array) {
+        if (backgroundColor) {
+            i.style.backgroundColor = backgroundColor;
+        }
+        if (textColor) {
+            i.style.color = textColor;
+        }
     }
-}
+};
+
+swap();
