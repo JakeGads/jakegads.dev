118fc3e94e4a02b3b5956cbaa3475438c6ff5092
Jake <jake.gads@gmail.com>
2020-06-05 22:29:56 -0400

    changed some stuff around but not much

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import faGem from '@fortawesome/fontawesome-free-regular/faGem'
+import logo from './logo';
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
@@ -10,9 +9,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Jake Gadaleta</h1>
+                <p>Full Stack Developer, Working with OSS</p>
             </div>
         </div>
         <nav>
