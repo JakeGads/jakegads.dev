1cbe8ca12e2e88031a7e1111164bc7e1bfd23729
Jake Gadaleta <jake.gads@gmail.com>
2020-12-17 15:30:31 -0500

    del package lock

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/jakeg/source/repo/jakegads.dev/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/jakeg/source/repo/jakegads.dev/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/jakeg/source/repo/jakegads.dev/src/pages/index.js"))),
   "component---src-pages-portifolio-js": hot(preferDefault(require("/Users/jakeg/source/repo/jakegads.dev/src/pages/portifolio.js")))
