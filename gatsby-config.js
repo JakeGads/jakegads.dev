a5ab3f4691cbef85b91afd51098f18529074d9a7
Jake Gadaleta <jake.gads@gmail.com>
2021-02-06 02:59:04 -0500

    syncing

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,10 @@ module.exports = {
       <ul>
         <li>Interning with Computer Aid Inc as a Full Stack Software Developer (Intern)</li>
         <li>Interning with Sigma AI as Data Scientist (Intern)</li>
+        <li>Working on a Python Music Manager</li>
         <li>Mentoring a FRC Robotics Team in Robot Programming</li>
+        <li>Managing two computer labs as a Senior Lab Tech @ DeSales</li>
+        <li>Working as a Senior Resident Advisor @ DeSales</li>
         <li>
           Currently I'm also working on the following projects
           <ul>
@@ -31,100 +34,182 @@ module.exports = {
         </li>
       </ul>
       <br>
-      <p>I'm also looking for a job</p>
+      <p>I'm also looking for a job, so you know ... If you believe that I may be a good match for your team please let me know</p>
       `,
     education: [
       {
-        degree: "Ph.D.",
+        degree: "Bachelor of Science",
         major: "Computer Science",
-        when: "2014-2018",
-        school: "Harvard University",
-        where: "Cambridge, MA",
-        moreInfo: `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        See my thesis <a href="#">here</a>.</p>`,
+        when: "2017-2021",
+        school: "DeSales University",
+        where: "Center Valley, PA",
+        moreInfo: `During my time at DeSales I maintained a ~3.75 major GPA (there is a semester left).
+        I completed the "PIC Math Research Study" in 2018 & have taken various Special Topics classes since that including "Intro To AI" and "Machine Learning"`,
       },
       {
-        degree: "M.Sc.",
-        major: "Software Engineering",
-        when: "2012-2014",
-        school: "Stanford University",
-        where: "Stanford, CA",
-        moreInfo: `"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."`,
+        degree: "Minor",
+        major: "Mathematics",
+        when: "2017-2021",
+        school: "DeSales University",
+        where: "Center Valley, PA",
       },
       {
-        degree: "B.Sc.",
-        major: "Computer Engineering",
-        when: "2008-2012",
-        school: "UCLA",
-        where: "Los Angeles, CA",
-        moreInfo: `<p>I finally found out how it feels like to live in LA; it was awesome!</p>`,
+        degree: "Minor",
+        major: "Data Science",
+        when: "2017-2021",
+        school: "DeSales University",
+        where: "Center Valley, PA",
+        moreInfo: `<p>I am the only Data Science Minor in the Class of 2021 and the first from DeSales University</p>`,
       },
       {
         degree: "High School Diploma",
-        when: "2000-2008",
-        school: "Awesome High School",
-        where: "Los Angeles, CA",
+        when: "2014-2017",
+        school: "Lansdale Catholic High School",
+        where: "Lansdale PA",
       },
     ],
     experience: [
       {
-        role: "Software Developer",
-        when: "2014-Present",
-        company: "Facebook",
-        where: "Menlo Park, CA",
-        moreInfo: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        role: "Development Intern",
+        when: "2021-Present",
+        company: "Sigma AI",
+        where: "Remote",
+        moreInfo: `I am working on an AI that will integrate with RASA 
+        in order to dynamically create new commands for various chatbot systems.`,
       },
       {
-        role: "Web Developer",
-        when: "2010-2014",
-        company: "Amazon",
-        where: "Vancouver, BC",
-        moreInfo: `See my <a href="#" target="_blank" rel="noopener noreferrer">work</a>.`,
+        role: "Mixtape Developer",
+        when: "2020-Present",
+        company: "Impossible Entertainment",
+        where: "Remote",
+        moreInfo: `I work with a team of ~10 people to create a python software product that incorporates
+        Machine Learning, Databases and Byte Manipulation in order to automatically repair DJ MP3 files.
+        Most of the team has never written code before which lead to this being a learning experience for all involved`,
+      },
+      {
+        role: "Full Stack Software Developer Intern",
+        when: "2019-Present",
+        company: "Computer Aid Inc",
+        where: "Allentown PA",
+        moreInfo: `I am part of the Intern Scrum Team, 
+        working on multiple projects that use a variety of tech stacks and frameworks, 
+        this role requires me to cover every aspect of the projects that we work on.`,
+      },
+      {
+        role: "Senior Lab Tech",
+        when: "2019-Present",
+        company: "DeSales University Computer Science Department",
+        where: "Center Valley PA",
+        moreInfo: `I manage 2 computer labs with approx. 75 computers. 
+        I handle updates, upgrades and security alongside maintaining software from a variety of venders. 
+        A large aspect of the role it to work along side students to make sure that their work can get done.
+        In 2021 I began to manage 2 new lab techs in the same role.`,
+      },
+      {
+        role: "Senior Resident Advisor",
+        when: "2019-Present",
+        company: "DeSales University Residence Life",
+        where: "Center Valley PA",
+        moreInfo: `I create schedules for fellow RA's (dynamically with code (repo is now proprietary)), 
+        and I work along side other Senior Resident Advisors to plan wide-scale overarching events this role.
+        I was promoted to Senior Resident Advisor in 2019, and compounded the responsibilities of the Resident Advisor Position (see below)`,
+      },
+      {
+        role: "Resident Advisor",
+        when: "2018-2019",
+        company: "DeSales University Residence Life",
+        where: "Center Valley PA",
+        moreInfo: `I work alongside other RAs to provide a safe, healthy, and fun environment for residents. 
+        This includes planning events/activities for residents on a monthly basis.`,
+      },
+      {
+        role: "Tune Up In PA Chairperson ",
+        when: "2019-Present",
+        company: "Lansdale Catholic Robotics",
+        where: "Lansdale PA",
+        moreInfo: `I created a python application that generates a schedule for 40+ teams, 
+        I also managed the judging portion of the competition and had 20+ people reporting to me.
+        The project is currently being rewritten to included new rules & train post highschool students on technical skills`,
+      },
+      {
+        role: "Head Student Ambassador",
+        when: "2018-2019",
+        company: "DeSales University IT Department",
+        where: "Center Valley PA",
+        moreInfo: `I generated schedules for other workers and gave monthly presentations that 
+        covered the technology used on campus for the faculty and staff.
+        I was promoted to this role in 2018 and it included all of the Student Ambassador responsibilities`,
+      },
+      {
+        role: "Student Ambassador",
+        when: "2017-2018",
+        company: "DeSales University IT Department",
+        where: "Center Valley PA",
+        moreInfo: `I maintained a campus wide IT system composed of various models and operating systems.`,
+      },
+      {
+        role: "Lead Mentor of Data Management and Analysis",
+        when: "2017-Present",
+        company: "Lansdale Catholic Robotics",
+        where: "Lansdale PA",
+        moreInfo: `In this volunteer position I teach highschool students to use statistical analysis to improve robotics competition results and guide them in using java/python to create tools to automate decision making during those competitions.`,
       },
     ],
     skills: [
       {
-        name: "JavaScript",
-        level: "85",
-        experience: "5 years",
-      },
-      {
         name: "Python",
-        level: "75",
-        experience: "2 years",
+        level: "85",
+        experience: "~8 years",
       },
       {
         name: "Java",
-        level: "65",
-        experience: "2 years",
+        level: "75",
+        experience: "~4 years",
       },
       {
-        name: "React",
+        name: "C#",
         level: "75",
-        experience: "1 years",
+        experience: "~3 years",
       },
       {
-        name: "Linux",
+        name: "Linux (Deb, RedHat, Arch)",
         level: "75",
-        experience: "4 years",
+        experience: "~10 years",
+      },
+      {
+        name: "Git (local, GitHub, Gitlab)",
+        level: "95",
+        experience: "~5 years",
       },
       {
         name: "C++",
-        level: "40",
-        experience: "1 years",
+        level: "65",
+        experience: "~2 years",
+      },
+      {
+        name: "Ruby",
+        level: "45",
+        experience: "~2 years",
+      },
+      {
+        name: "Go-lang",
+        level: "35",
+        experience: "~1 year",
+      },
+      {
+        name: "Docker",
+        level: "35",
+        experience: "<1 year",
       },
     ],
     interests: [
       "Reading",
       "Programming",
-      "Playing the violin",
-      "Running",
-      "Watching Monty Python and the Holy Grail",
+      "Teaching",
+      "Learning",
+      "Networking",
+      "Listening to Podcasts",
+      "Watching Scott Pilgrim vs the World (the books are better)",
     ],
     //available_themes: ["great-gatsby", "master-yoda", "wonder-woman", "darth-vader", "luke-lightsaber"],
     theme: "great-gatsby",
